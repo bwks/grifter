e01e2f77ca69f259818d2850fabd187938f160e5
bobthebutcher <bradleysearle@gmail.com>
2018-04-19 23:11:05 +1000

    tempalte loaders, host data, flux capacitors

--- utils/custom_filters.py
+++ utils/custom_filters.py
@@ -1,16 +1,3 @@
-import random
-
-
-def get_mac(oui='28:b7:ad'):
-    """
-    Generate a random MAC address.
-    param oui: MAC address OUI
-    return: MAC address as a string
-    """
-    nic = ':'.join([format(random.randint(0, 255), '02x') for i in range(0, 3)])
-    return '{0}:{1}'.format(oui, nic)
-
-
 def explode_port(port):
     """
     Create a high port number > 10000 for use with UDP tunnels
