2c93fc9cda67ffeb6ebf408b1338eb102c1ff18b
bobthebutcher <bradleysearle@gmail.com>
2018-04-22 10:04:22 +1000

    adding cli

--- toolbelt/cli.py
+++ toolbelt/cli.py
@@ -7,14 +7,9 @@ from toolbelt import generate_vagrant_file
 
 
 @click.command()
-@click.option('--build', default=True, help='Build Vagrantfile')
 @click.argument('data_file')
 def cli(data_file):
     data = load_host_data(data_file)
     loopbacks = generate_loopbacks(data['hosts'])
     update_hosts(data['hosts'])
     return generate_vagrant_file(data, loopbacks)
-
-
-if __name__ == '__main__':
-    cli()
