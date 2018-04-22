ca6a9d2269d6a95aabb9d0c15c3503111f7c604e
bobthebutcher <bradleysearle@gmail.com>
2018-04-22 10:13:21 +1000

    adding cli

--- toolbelt/cli.py
+++ toolbelt/cli.py
@@ -9,6 +9,11 @@ from toolbelt import generate_vagrant_file
 @click.command()
 @click.argument('DATAFILE')
 def cli(data_file):
+    """
+    Create a Vagrantfile from a yaml file.
+    :param data_file: Location of data_file
+    :return: Vagrantfile
+    """
     data = load_host_data(data_file)
     loopbacks = generate_loopbacks(data['hosts'])
     update_hosts(data['hosts'])
