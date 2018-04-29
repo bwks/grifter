f2bc8dc65f8e4d127a26bba80c200284e1723664
bobthebutcher <bradleysearle@gmail.com>
2018-04-29 16:49:09 +1000

    test utils

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from vagrantfile_builder.utils import get_mac
+
+
+def test_get_mac_returns_default_oui():
+    assert get_mac()[0:8] == '28:b7:ad'
+
+
+def test_get_mac_returns_custom_oui():
+    assert get_mac('00:00:00')[0:8] == '00:00:00'
