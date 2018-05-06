08bb839d867f756b0f0168ae6fc269bebe1e875d
Brad Searle <bobthebutcher@users.noreply.github.com>
2018-05-06 20:24:02 +1000

    Fixes for #15 (#16)
    * test constants
    
    * test utils
    
    * test api
    
    * add some comments and fix indent
    
    * add some comments and fix indent
    
    * more tests
    
    * tests
    
    * add version file
    
    * add version file
    
    * api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,10 @@
+import copy
 import pytest
 
 from vagrantfile_builder.api import (
     load_host_data,
     generate_loopbacks,
+    update_hosts,
     update_interfaces,
 )
 
@@ -38,6 +40,65 @@ def test_generate_loopbacks_returned_loopback_dict():
         'sw02': '127.255.1.2'
     }
     assert generate_loopbacks(mock_load_host_data['hosts']) == expected_loopback_dict
+
+
+def test_host_without_interfaces():
+    expected = {
+        'hosts': [
+            {
+                'insert_ssh_key': False,
+                'name': 'sw01',
+                'provider_config': {
+                    'cpus': 2,
+                    'disk_bus': 'ide',
+                    'management_network_mac': None,
+                    'memory': 2048,
+                    'nic_adapter_count': 2
+                },
+                'synced_folder': None,
+                'vagrant_box': {
+                    'name': 'arista/veos',
+                    'provider': 'libvirt',
+                    'version': None
+                }
+            },
+            {
+                'insert_ssh_key': False,
+                'interfaces': [
+                    {
+                        'local_port': 1,
+                        'name': 'eth1',
+                        'remote_host': 'sw01',
+                        'remote_port': 1
+                    },
+                    {
+                        'local_port': 2,
+                        'name': 'eth2',
+                        'remote_host': 'sw01',
+                        'remote_port': 2
+                    }
+                ],
+                'name': 'sw02',
+                'provider_config': {
+                    'cpus': 2,
+                    'disk_bus': 'ide',
+                    'management_network_mac': None,
+                    'memory': 2048,
+                    'nic_adapter_count': 2
+                },
+                'synced_folder': None,
+                'vagrant_box': {
+                    'name': 'arista/veos',
+                    'provider': 'libvirt',
+                    'version': None
+                }
+            }
+        ]
+    }
+
+    hosts = copy.deepcopy(mock_load_host_data)
+    hosts['hosts'][0].pop('interfaces')
+    assert update_hosts(hosts['hosts']) == expected['hosts']
 
 
 def test_update_interfaces_with_same_interface_count_returns_same_list_of_interfaces():
