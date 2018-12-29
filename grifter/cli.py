c99e02a2dd299ed1e9748f8fafc022d6d9326f6f
Brad Searle <bradleysearle@gmail.com>
2018-12-29 21:27:27 +1000

    version bump

--- grifter/cli.py
+++ grifter/cli.py
@@ -125,7 +125,7 @@ def display_connections(connections_list, guest=''):
 
 
 @click.group(context_settings={'help_option_names': ['-h', '--help']})
-@click.version_option(version='0.2.7')
+@click.version_option(version='0.2.8')
 def cli():
     """Create a Vagrantfile from a YAML data input file."""
     pass
