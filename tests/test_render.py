d089513429ea61a7028ec803c6ac2282e76395cf
Brad Searle <bradleysearle@gmail.com>
2018-12-31 18:12:08 +1000

    add tests

--- tests/test_render.py
+++ tests/test_render.py
@@ -52,3 +52,42 @@ def test_render_vagrantfile_with_additional_storage_interfaces(mock_storage_size
     )
 
     assert mock_vagrantfile_with_additional_storage_volumes == vagrantfile
+
+
+def test_blackhole_interfaces_trigger_rendering():
+    guest = 'sw01'
+    blackhole_interfaces = {'sw01': ['swp1', 'swp2']}
+    expected = """
+    blackhole_interfaces = ["swp1", "swp2"]
+    blackhole_interfaces.each do |interface|
+      node.trigger.after :up do |trigger|
+        trigger.info = "Shutting down #{guest_name}-#{interface}"
+        trigger.run = {inline: "virsh domif-setlink #{domain_prefix}_#{guest_name} #{guest_name}-#{interface}-#{domain_uuid} down"}
+      end
+    end
+"""
+    result = render_from_template(
+        template_name='blackhole-interfaces-trigger.j2',
+        template_directory=TEMPLATES_DIR,
+        guest=guest,
+        blackhole_interfaces=blackhole_interfaces,
+    )
+    assert result == expected
+
+
+def test_throttle_cpu_trigger_rendering():
+    data = {
+        'vagrant_box': {'throttle_cpu': 33}
+    }
+    expected = """
+    node.trigger.after :up do |trigger|
+      trigger.info = "Throttling #{domain_prefix}_#{guest_name} CPU"
+      trigger.run = {inline: "virsh schedinfo #{domain_prefix}_#{guest_name} --set vcpu_quota=33000"}
+    end
+"""
+    result = render_from_template(
+        template_name='throttle-cpu-trigger.j2',
+        template_directory=TEMPLATES_DIR,
+        data=data,
+    )
+    assert result == expected
